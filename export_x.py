a568680579990f0420608507d5b9954b2578d440
fred-rum <fred-rum@users.noreply.github.com>
2019-01-14 23:19:41 -0800

    Write fewer vertexes when exporting skin weights
    Faces can share vertexes just fine when exporting skin weights.  There
    is no need to unroll faces.

--- export_x.py
+++ export_x.py
@@ -518,9 +518,8 @@ class MeshExportObject(ExportObject):
 
         # Create the mesh enumerator based on options
         MeshEnumerator = None
-        if (self.Config.ExportUVCoordinates and Mesh.uv_textures) or \
-            (self.Config.ExportVertexColors and Mesh.vertex_colors) or \
-            (self.Config.ExportSkinWeights):
+        if ((self.Config.ExportUVCoordinates and Mesh.uv_textures) or
+            (self.Config.ExportVertexColors and Mesh.vertex_colors)):
             MeshEnumerator = MeshExportObject._UnrolledFacesMeshEnumerator(Mesh)
         else:
             MeshEnumerator = MeshExportObject._OneToOneMeshEnumerator(Mesh)
